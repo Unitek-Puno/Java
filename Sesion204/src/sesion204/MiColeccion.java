0a1656a51855059dd454f60d7ad41d8569b3a1fa
fr3dygit <f@unap.edu.pe>
2014-10-20 20:41:43 -0500

    Creamos los atributos y el constructor para la clase MiColeccion

--- Sesion204/src/sesion204/MiColeccion.java
+++ Sesion204/src/sesion204/MiColeccion.java
@@ -10,5 +10,11 @@ package sesion204;
  * @author fr3dy
  */
 public class MiColeccion {
+    private Object datos[] = null;
+    private int len = 0;
+    
+    public MiColeccion (int capacidadInicial) {
+        datos = new Object[capacidadInicial];
+    }
     
 }
