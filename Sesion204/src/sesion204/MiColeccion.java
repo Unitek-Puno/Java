c0206e131fe8cf0c2175bd9b4c2172f5c1c232d1
fr3dygit <f@unap.edu.pe>
2014-10-20 20:47:01 -0500

    Ordenamos los comentarios y espacios

--- Sesion204/src/sesion204/MiColeccion.java
+++ Sesion204/src/sesion204/MiColeccion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sesion204;
 
 /**
@@ -10,6 +5,7 @@ package sesion204;
  * @author fr3dy
  */
 public class MiColeccion {
+
     private Object datos[] = null;
     private int len = 0;
     
